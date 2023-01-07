66e68f5dc7c5f5988bdcb8ce6a214345c58f28a1
Alec Schrader <alecschrader@gmail.com>
2023-01-07 15:48:00 -0500

    wireframing for new/join comp

--- src/pages/JoinComp.js
+++ src/pages/JoinComp.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, Container, dividerClasses, Grid, Item } from "@mui/material";
+import { Container, Box, TextField } from "@mui/material";
 
 
 export default function JoinComp() {
@@ -9,6 +9,34 @@ export default function JoinComp() {
             alignItems:'center'
           }}>
             <h1>Have a code?</h1>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField
+                    required
+                    id="compcode"
+                    label="Competition Code"
+                    helperText="The code you recieved in your invitation."
+                />
+                <TextField
+                    required
+                    id="displayname"
+                    label="Display Name"
+                    helperText="The name you would like to use in the competition."
+                />
+                <TextField
+                    required
+                    type={"password"}
+                    id="password"
+                    label="Password"
+                    helperText="This will be how you access this competition in the future."
+                />
+            </Box>
         </Container>
     );
 };  
